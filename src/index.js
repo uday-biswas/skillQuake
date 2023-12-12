e3d8d7dea5778e06755611bca92db8a503d970ab
uday biswas <ub8280634471@gmail.com>
2023-12-12 17:14:55 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers";
+import { Toaster } from "react-hot-toast";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <Toaster
+        position="bottom-center"
+        containerClassName=""
+        toastOptions={{
+          className: "bg-richblack-5 font-semibold rounded-md",
+          duration: 4000,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+          success: {
+            duration: 3000,
+            className: "border-b-[5px] border-caribbeangreen-400 font-semibold",
+          },
+
+          error: {
+            duration: 5000,
+            className: "border-b-[5px] border-pink-50 font-semibold",
+          },
+
+          loading: {
+            duration: 3500,
+            className: "border-b-[5px] border-richblack-700 font-semibold",
+          },
+        }}
+      />
+    </BrowserRouter>
+  </Provider>
+);
